69b4aaa71c1d1c4bbaf2dc992bcc90473de62f9d
Colibri91 <mertdolar@gmail.com>
2022-03-11 15:15:41 +0300

    - Color imp.

---
+++ CoffeeApp/Extensions/Colors.swift
@@ -0,0 +1,22 @@
+//
+//  Colors.swift
+//  CoffeeApp
+//
+//  Created by Rıfat Mert Dolar on 11.03.2022.
+//
+
+import SwiftUI
+
+struct Colors{
+    
+    static let lightBlue = Color(red: 51 / 255, green: 153 / 255, blue: 255 / 255)
+
+    static let paleGreen = Color(red: 0 / 255, green: 128 / 255, blue: 0 / 255)
+    
+}
+
+extension Color {
+    
+    static let coffeeBrown = Color("coffeeBrown")
+
+}
